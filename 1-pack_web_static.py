80a9d27f23a0bece4a7e8980908bb3b79abe4011
talk2dennis <adigwedennis@gmail.com>
2024-04-04 07:35:18 +0100

    fixed pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,15 +10,15 @@ from datetime import datetime
 
 def do_pack():
     """
-        a fabfile task to pack the contents of folder into .tgz
+        a fabfile to pack the contents of folder into .tgz
     """
 
     # get the current time
     time = datetime.now().strftime("%Y%m%d%H%M%S")
-    
+
     local("mkdir -p versions")
     result = local(f"tar -cvzf versions/web_static_{time}.tgz web_static/",
-                   capture = True)
+                   capture=True)
     if result.succeeded:
         return f"versions/web_static_{time}.tgz"
     else:
