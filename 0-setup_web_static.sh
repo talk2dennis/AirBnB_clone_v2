8e50f23b1fece46a1711e8e624396b48451b3df5
talk2dennis <adigwedennis@gmail.com>
2024-04-04 05:51:39 +0100

    a bash script to reprovision our server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+
+# html content
+page="\
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+
+sudo apt -y update
+sudo apt -y install nginx
+
+# create the necessary folders
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+# create fake html file 
+echo $page | sudo tee "/data/web_static/releases/test/index.html" > "/dev/null"
+
+# check if file exists and delete
+if [ -f /data/web_static/current ]; then
+	rm /data/web_static/current
+fi
+
+# create a symbolic link
+ln -s /data/web_static/current /data/web_static/releases/test/
+
+# give file ownership to ubuntu
+chown -R ubuntu:ubuntu /data
+
+# using alias to state the location of the static content for images
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# reload nginx
+sudo service nginx restart
