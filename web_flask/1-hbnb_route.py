0e412478021605ce25b55c5ed4fc6c8c376f6a88
talk2dennis <adigwedennis@gmail.com>
2024-04-18 21:16:29 +0100

    fixed pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,10 +3,11 @@
     script that starts a Flask web application:
     Your web application must be listening on 0.0.0.0, port 5000
 """
-
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
