41489564d42a9ee291b81cf3b1e261284cfce92e
talk2dennis <adigwedennis@gmail.com>
2024-04-18 21:00:46 +0100

    flask to accept variable passed to the route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    display "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display "HBNB"
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    display "C" followed by the text passed as an argurement
+    """
+    name = text.replace('_', ' ')
+    return f"C {name}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
