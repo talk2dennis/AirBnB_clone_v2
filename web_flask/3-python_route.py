aa82a1dedc143d834d72ee7aee1e58f281b700d7
talk2dennis <adigwedennis@gmail.com>
2024-04-18 21:14:56 +0100

    flask route to accept paramaters passed

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    display "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    display "C" followed by the text passed as an argurement
+    """
+    name = text.replace('_', ' ')
+    return f"C {name}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python(text="is cool"):
+    """
+    python: display “Python ”, followed by the value of the text variable
+            (replace underscore _ symbols with a space)
+    """
+    name = text.replace('_', ' ')
+    return f"Python {name}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
