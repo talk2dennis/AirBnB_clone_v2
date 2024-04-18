965811a936c4c77a78d4157749921572e033b444
talk2dennis <adigwedennis@gmail.com>
2024-04-18 21:15:42 +0100

    fixed code style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,15 @@
     script that starts a Flask web application
 """
 
+
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def home():
     """ home page"""
     return "Hello HBNB!"
-
 
 
 if __name__ == '__main__':
